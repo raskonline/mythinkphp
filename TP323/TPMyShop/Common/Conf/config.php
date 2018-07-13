415da0d1a113345973f75bc54bd094e933302397
rask <2083967667@qq.com>
2018-07-14 07:31:28 +0800

    跟踪配置

--- TP323/TPMyShop/Common/Conf/config.php
+++ TP323/TPMyShop/Common/Conf/config.php
@@ -1,4 +1,8 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    //'配置项'=>'配置值'
+    //配置页面追踪
+    'SHOW_PAGE_TRACE' => true,
+    'DEFAULT_MODULE' => 'Admin',  // 默认模块
+    'MODULE_ALLOW_LIST' => array('Home', 'Admin')
 );
