434df44c3a3a84a0bcb3b913df522f1ce353f349
rask <2083967667@qq.com>
2018-07-20 00:55:50 +0800

    模型和数据表

--- TP323/TPMyShop/Home/Controller/UserController.class.php
+++ TP323/TPMyShop/Home/Controller/UserController.class.php
@@ -9,17 +9,46 @@
 namespace Home\Controller;
 
 
+use Model\UserModel;
 use Think\Controller;
 
 class UserController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
         $this->display("login");
     }
 
-    public function login(){
+    public function login()
+    {
         $this->display();
+    }
+
+    public function register()
+    {
+        $userModel = new UserModel();
+        if (!empty($_POST)) {
+            $_POST['user_hobby'] = implode(",", $_POST['user_hobby']);
+
+            if (!$userModel->create($_POST)) {//验证不通过
+
+                $errinfo = $userModel->getError();
+                $this->assign("errinfo", $errinfo);
+            } else {//验证通过
+                $result = $userModel->add($_POST);
+                if ($result) {
+                    $this->redirect("login", array(), 2, "<h1>:)</h1>用户注册成功！");
+                } else {
+                    $this->redirect("register", array(), 2, "<h1>:(</h1>用户注册失败！");
+                }
+            }
+
+
+        }
+        $this->display();
+
+
     }
 
 }
---
+++ TP323/TPMyShop/Model/UserModel.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018-07-19
+ * Time: 23:41
+ */
+
+namespace Model;
+
+
+use Think\Model;
+
+class UserModel extends Model
+{
+    protected $_validate = array(
+        array('username', 'require', '用户名不允许重复！', 0, 'unique', 1),
+        array('password', 'require', '密码不允许为空！'),
+        array('password2', 'require', '确认密码不允许为空！'),
+        array('password2', 'password', '两次输入密码不一致！', 0, 'confirm'),
+        array('user_email', 'email', '邮箱格式不正确！'),
+        array('user_qq', 'number', 'QQ号必须是数字！'),
+        array('user_qq', '5,12', 'QQ位数不正确！'),
+        array('user_tel', '/^1[3|4|5|8|9][0-9]\d{8}$/', '手机号格式不正确！', 0, 'regex', 1)
+    );  // 自动验证定义
+
+    protected $patchValidate = true;
+}
