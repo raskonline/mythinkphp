345e204c2c302df32fc864fe54e2c26acb27c634
rask <2083967667@qq.com>
2018-07-20 14:39:56 +0800

    Merge branch 'master' of https://github.com/raskonline/mythinkphp
    # Conflicts:
    #	TP323/TPMyShop/Admin/Controller/GoodsController.class.php

--- TP323/TPMyShop/Model/UserModel.class.php
+++ TP323/TPMyShop/Model/UserModel.class.php
@@ -20,7 +20,7 @@ class UserModel extends Model
         array('password2', 'password', '两次输入密码不一致！', 0, 'confirm'),
         array('user_email', 'email', '邮箱格式不正确！'),
         array('user_qq', 'number', 'QQ号必须是数字！'),
-        array('user_qq', '5,12', 'QQ位数不正确！'),
+        array('user_qq', '5,12', 'QQ位数不正确！',0,'length',1),
         array('user_tel', '/^1[3|4|5|8|9][0-9]\d{8}$/', '手机号格式不正确！', 0, 'regex', 1)
     );  // 自动验证定义
 
